e7d769459d6eb9524ab89ab611108d841f932efc
Aamir Zuhail Basheer <amruazb@gmail.com>
2025-05-10 19:44:56 +0400

    new deploy 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: ['api.intra.42.fr', 'cdn.intra.42.fr'],
     unoptimized: true,
